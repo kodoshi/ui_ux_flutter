c228b7ca05fe9ab2ade3419ff6ba757c473622f7
VictorTHOMAS94 <victor.thomas@epitech.eu>
2021-10-30 13:10:08 +0200

    [add] amount page with textfield and dropdown menu for currency | unfinish

---
+++ mobile/banking_app/lib/pages/amount_page.dart
@@ -0,0 +1,142 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:banking_app/pages/component/dropdown.dart';
+
+
+class AmountPage extends StatefulWidget {
+  const AmountPage({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _AmountPageState();
+  }
+}
+
+class _AmountPageState extends State<AmountPage> {
+  final _formKey = GlobalKey<FormState>();
+  bool _enableBtn = false;
+  TextEditingController emailController = TextEditingController();
+  TextEditingController subjectController = TextEditingController();
+  TextEditingController messageController = TextEditingController();
+  
+    List<DropdownMenuItem<String>> get dropdownItems{ 
+    List<DropdownMenuItem<String>> menuItems = [
+      DropdownMenuItem(child: Text("USA"),value: "USA"),
+      DropdownMenuItem(child: Text("Canada"),value: "Canada"),
+      DropdownMenuItem(child: Text("Brazil"),value: "Brazil"),
+      DropdownMenuItem(child: Text("England"),value: "England"),
+    ];
+    return menuItems;
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    emailController.dispose();
+    subjectController.dispose();
+    messageController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Amounts"),
+      ),
+      body: Form(
+        key: _formKey,
+        onChanged: (() {
+          setState(() {
+            _enableBtn = _formKey.currentState!.validate();
+          });
+        }),
+        child: SingleChildScrollView(
+          child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          // mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              const SizedBox(
+                height: 100.0,
+              ),
+               Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Available :", style: TextStyle(color: Colors.black.withOpacity(0.8)),
+                    ),
+                    const Text("£ 3,150.70", style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                  ]
+                ),
+                const DropdownMenu(),              
+              Container(
+                margin: const EdgeInsets.only(top: 10.0),
+                alignment: Alignment.center,
+                child: 
+                    TextField(
+                      decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: '00,00.00'
+                    ),
+                    style: const TextStyle(
+                    fontSize: 42.0,
+                      ),
+                    textAlign: TextAlign.center,
+                    keyboardType: TextInputType.number,
+                    inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly
+                    ],
+                  ),
+              ),
+              
+               Container(
+                  margin: const EdgeInsets.only(top: 60.0),
+                  alignment: Alignment.center,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Commission :", style: TextStyle(color: Colors.black.withOpacity(0.8)),
+                      ),
+                      const Text("£ 3.30", style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ]
+                  ),
+                ),
+             Container(
+                height: 50.0,
+                margin: const EdgeInsets.only(top: 80.0),
+                alignment: Alignment.bottomCenter,
+                child: RaisedButton(
+                onPressed: () {},
+                padding: EdgeInsets.all(0.0),
+                child: Ink(
+                  decoration: const BoxDecoration(
+                     gradient:  LinearGradient(
+                       colors: [ Color(0xff374ABE), Color(0xff64B6FF)],
+                       begin: Alignment.centerLeft,
+                       end: Alignment.centerRight,
+                     ),
+                ),
+                 child: Container(
+                   constraints:
+                       const BoxConstraints(minHeight: 50.0),
+                   alignment: Alignment.center,
+                   child: const Text(
+                     "Send money",
+                     textAlign: TextAlign.center,
+                     style: TextStyle(color: Colors.white, fontSize: 15),
+                   ),
+                 ),
+               ),
+             ),
+           ),
+           
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ mobile/banking_app/lib/pages/component/dropdown.dart
@@ -0,0 +1,50 @@
+/// Flutter code sample for DropdownButton
+
+// This sample shows a `DropdownButton` with a large arrow icon,
+// purple text style, and bold purple underline, whose value is one of "One",
+// "Two", "Free", or "Four".
+//
+// ![](https://flutter.github.io/assets-for-api-docs/assets/material/dropdown_button.png)
+
+import 'package:flutter/material.dart';
+
+
+/// This is the stateful widget that the main application instantiates.
+class DropdownMenu extends StatefulWidget {
+  const DropdownMenu({Key? key}) : super(key: key);
+
+  @override
+  State<DropdownMenu> createState() => _DropdownMenuState();
+}
+
+/// This is the private State class that goes with DropdownMenu.
+class _DropdownMenuState extends State<DropdownMenu> {
+  String dropdownValue = '£';
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: dropdownValue,
+      icon: const Icon(Icons.arrow_downward),
+      iconSize: 24,
+      elevation: 16,
+      style: const TextStyle(color: Colors.deepPurple),
+      underline: Container(
+        height: 2,
+        color: Colors.deepPurpleAccent,
+      ),
+      onChanged: (String? newValue) {
+        setState(() {
+          dropdownValue = newValue!;
+        });
+      },
+      items: <String>['£','\$','€','¥']
+          .map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+    );
+  }
+}
